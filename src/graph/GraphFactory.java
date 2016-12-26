2013010e158234eada67430ed4f638939ee71f79
pawelkopec <pawelkopec10@gmail.com>
2016-12-26 18:52:45 +0100

    GraphFactory: add creating instances from string and stream
    From now on - change message style to:
    Packgage/Class: Issue

--- src/graph/GraphFactory.java
+++ src/graph/GraphFactory.java
@@ -1,9 +1,15 @@
 package graph;
 
+import java.io.InputStream;
+
 /**
  * Created by Paweł Kopeć on 26.12.16.
  *
  * Class for automation of Graph subclasses constructing
+ *
+ *TODO
+ * Think of some meta-programming to get rid of ugly repeating
+ * of if-else and switch statements in different getInstance(...).
  */
 public class GraphFactory {
 
@@ -31,5 +37,39 @@ public class GraphFactory {
 
     public static Graph getInstance(String className) throws ClassNotFoundException, IllegalArgumentException {
         return GraphFactory.getInstance(className, 0);
+    }
+
+    public static Graph getInstanceFromString(String className, String string) throws ClassNotFoundException, IllegalArgumentException {
+        Class graphClass = Class.forName(className);
+
+        if (Graph.class.isAssignableFrom(graphClass)) {
+            switch (graphClass.getName()) {
+                case "graph.ListGraph":
+                    return new ListGraph(string);
+                default:
+                    throw new IllegalArgumentException(className + " cannot be initialized as Graph.");
+
+            }
+        } else {
+            throw new IllegalArgumentException(className + " cannot be initialized as Graph.");
+        }
+    }
+
+    public static Graph getInstanceFromStream(String className, InputStream in) throws ClassNotFoundException, IllegalArgumentException {
+        Class graphClass = Class.forName(className);
+
+        if (Graph.class.isAssignableFrom(graphClass)) {
+            switch (graphClass.getName()) {
+                case "graph.ListGraph":
+                    return new ListGraph(in);
+                case "graph.MatrixGraph":
+                    return new MatrixGraph(in);
+                default:
+                    throw new IllegalArgumentException(className + " cannot be initialized as Graph.");
+
+            }
+        } else {
+            throw new IllegalArgumentException(className + " cannot be initialized as Graph.");
+        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,20 @@
 package main;
 
 import graph.Graph;
-import graph.GraphFactory;
 import graph.ListGraph;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException {
-        Tester.runAllTests();
-        /*Graph g = GraphFactory.getInstance("graph.ListGraph", 7);
-        g.addEdge(3, 4);
-        g.addEdge(0, 2);
-        g.addEdge(3, 1);
-        g.addEdge(2, 3);
-        g.addEdge(1, 2);
-        g.addEdge(1, 4);
+        //Tester.runAllTests();
+        String s = "7 6 3 4 0 2 3 1 2 3 1 2 1 4";
+        Graph g = new ListGraph(s); //GraphFactory.getInstance("graph.ListGraph", 7);
         System.out.println(g);
         g.removeEdge(1, 3);
         System.out.println(g);
-        g.makeEmpty();*/
+        g.makeEmpty();
     }
 }
