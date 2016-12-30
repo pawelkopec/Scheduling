8f6107b826e694cf31d539d21b39b36980165523
pawelkopec <pawelkopec10@gmail.com>
2016-12-31 00:09:14 +0100

    Replace Colorable interface with VertexColoring class
    Add a "client" for graph coloring. It's a much better idea then Colorable interface since it separates abstraction of coloring from abstraction of graph operations.

---
+++ src/graph/VertexColoring.java
@@ -0,0 +1,56 @@
+package graph;
+
+import graph.Graph;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Paweł Kopeć on 30.12.16.
+ *
+ * VertexColoring class provides an interface
+ * for assigning colors to graph's vertices
+ * and dividing them into color classes.
+ */
+public class VertexColoring {
+
+    private Graph graph;
+    private int verticesNumber;
+    private int[] colors;
+
+    public VertexColoring(Graph graph) {
+        this.graph = graph;
+        verticesNumber = graph.getVerices();
+        colors = new int[verticesNumber];
+    }
+
+    /**
+     * Set color of a given vertex.
+     *
+     * @param index of a vertex to be colored
+     * @param color that is to be assigned to a vertex
+     */
+    void setColor(int index, int color) {
+        graph.validateVertex(index);
+        colors[index] = color;
+    }
+
+    /**
+     * Get color of a given vertex.
+     *
+     * @param index of a given vertex
+     * @return color of a given vertex
+     */
+    int getColor(int index) {
+        graph.validateVertex(index);
+        return colors[index];
+    }
+
+    /**
+     * Get array of colors.
+     *
+     * @return array of colors
+     */
+    int[] getColors() {
+        return colors;
+    }
+}
