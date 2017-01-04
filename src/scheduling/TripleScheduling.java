8c889451021f7ba03b5b85e8d0666e870e0a69fe
pawelkopec <pawelkopec10@gmail.com>
2017-01-05 00:09:02 +0100

    TripleSchedulng: add findOptimalDivision()
    For case when graph is 2-chromatic and s1 < s2 + s3 for s1 >= s2 >= s3.

--- src/scheduling/TripleScheduling.java
+++ src/scheduling/TripleScheduling.java
@@ -5,7 +5,10 @@ import graph.VertexColoring;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Paweł Kopeć on 28.12.16.
@@ -39,19 +42,14 @@ public class TripleScheduling {
     private ListGraph graph;
     private VertexColoring coloring;
     private double[] speeds;
+    private double sumOfSpeeds;
     private int state = NOT_CHECKED;
-
-    public TripleScheduling(ListGraph graph) {
-        this.graph = graph;
-        this.speeds = new double[]{1., 1., 1.};
-        this.coloring = new VertexColoring(graph);
-    }
 
     public TripleScheduling(ListGraph graph, double[] speeds) {
         setSpeeds(speeds);
         this.graph = graph;
-        this.speeds = speeds;
-        this.coloring = new VertexColoring(graph);
+        for(double i : speeds) sumOfSpeeds += i;
+        coloring = new VertexColoring(graph);
     }
 
     /**
@@ -85,32 +83,37 @@ public class TripleScheduling {
         if(state == NOT_CHECKED) {
             checkState();
         }
-
+        findOptimalDivision();
         switch(state) {
             case BRUTE_FORCE:
             case BRUTE_FORCE_EASY:
-                findBruteForce(speeds);
+                findBruteForce();
                 break;
             case OPTIMAL:
-                findOptimal(speeds);
+                findOptimal();
                 break;
             case SUBOPTIMAL:
-                findSuboptimal(speeds);
+                findSuboptimal();
                 break;
         }
 
         return coloring;
     }
 
-    private void findOptimal(double[] speeds) {
+    private void findOptimal() {
+        if(speeds[2] < speeds[1] + speeds[2]) {
+            CLW(findOptimalDivision());
+        }
+        else {
+            int n2 = (int)Math.ceil(0.5 * speeds[1]/(speeds[0] + speeds[1]));
+        }
+    }
+
+    private void findSuboptimal() {
         //TODO
     }
 
-    private void findSuboptimal(double[] speeds) {
-        //TODO
-    }
-
-    private void findBruteForce(double[] speeds) {
+    private void findBruteForce() {
         //TODO
     }
 
@@ -122,7 +125,7 @@ public class TripleScheduling {
      */
     private boolean is2chromatic() {
         Queue<Integer> queue = new LinkedList<>();
-        int current = 0, currentColor = 1, otherColor = 2, noColor = 0, tempColor;
+        int current = 0, currentColor = 1, otherColor, noColor = 0, tempColor;
 
         queue.add(current);
         coloring.setColor(current, currentColor);
@@ -145,6 +148,49 @@ public class TripleScheduling {
         }
 
         return true;
+    }
+
+    int[] findOptimalDivision() {
+        int n = graph.getVertices();
+        double[] nFloat = new double[3];
+        for(int i = 0; i < speeds.length; i++) {
+            nFloat[i] = n * speeds[i] / sumOfSpeeds;
+        }
+
+        double time2Floor = Math.floor(nFloat[1]) / speeds[1];
+        double time2Ceil= Math.ceil(nFloat[1]) / speeds[1];
+        double time3Floor = Math.floor(nFloat[2]) / speeds[2];
+        double time3Ceil= Math.ceil(nFloat[2]) / speeds[2];
+        double totalTime = n / sumOfSpeeds;
+
+
+        double maxTime1 = Math.max(time3Floor, Math.max(time2Ceil, totalTime - time3Floor - time2Ceil));
+        double maxTime2 = Math.max(time3Ceil, Math.max(time2Floor, totalTime - time3Ceil - time2Floor));
+        double maxTime3 = Math.max(time3Ceil, Math.max(time2Ceil, totalTime - time3Ceil - time2Ceil));
+
+        double minTime = Math.min(maxTime1, Math.min(maxTime2, maxTime3));
+
+        int[] division = new int[3];
+
+        if(minTime == maxTime1) {
+            division[2] = (int)Math.floor(nFloat[2]);
+            division[1] = (int)Math.ceil(nFloat[1]);
+        }
+        else if(minTime == maxTime2) {
+            division[2] = (int)Math.ceil(nFloat[2]);
+            division[1] = (int)Math.floor(nFloat[1]);
+        }
+        else {
+            division[2] = (int)Math.ceil(nFloat[2]);
+            division[1] = (int)Math.ceil(nFloat[1]);
+        }
+        division[0] = n - division[1] - division[2];
+
+        assertEquals(division[0] + division[1] + division[2], n);
+        System.out.println(Arrays.toString(division));
+        System.out.println(Arrays.toString(speeds));
+
+        return division;
     }
 
     /**
@@ -178,5 +224,6 @@ public class TripleScheduling {
         }
 
         Arrays.sort(speeds);
+        this.speeds = speeds;
     }
 }
