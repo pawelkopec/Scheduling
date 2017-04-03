96c2c539e5153a89d9c4171307edeb641c322b3d
pawelkopec <pawelkopec10@gmail.com>
2017-04-03 10:57:42 +0200

    ComponentSwapper: add comments

--- src/scheduling/three/ComponentSwapper.java
+++ src/scheduling/three/ComponentSwapper.java
@@ -9,7 +9,7 @@ import java.util.Queue;
 
 /**
  * Created by Paweł Kopeć on 15.03.17.
- *
+ * <p>
  * Class used for swapping colors between components
  * within clw procedure.
  */
@@ -29,17 +29,17 @@ class ComponentSwapper {
      * If difference in sizes of the components are too big
      * compensate it by moving vertices from compensator.
      *
-     * @param colorBig color of bigger color class
-     * @param colorSmall color of smaller color class
-     * @param verticesToMove desired change of coloring  width
-     * @return how much width was decreased
+     * @param colorBig       color of bigger color class
+     * @param colorSmall     color of smaller color class
+     * @param verticesToMove desired change of the coloring width
+     * @return how much the width was decreased
      */
     int swap(int colorBig, int colorSmall, int verticesToMove, LinkedList<Integer> compensator) {
         BitSet checked = new BitSet(graph.getVertices());
         LinkedList<Integer> bigComponent = new LinkedList<>(), smallComponent = new LinkedList<>();
         int currentColor, sizeDifference, toCompensate, verticesMoved = 0;
 
-        for(int i = 0; i < graph.getVertices(); i++) {
+        for (int i = 0; i < graph.getVertices(); i++) {
 
             if (!checked.get(i) && 0 < verticesToMove) {
 
@@ -69,12 +69,22 @@ class ComponentSwapper {
         return verticesMoved;
     }
 
+    /**
+     * Try to swap components between color classes
+     * so that the sizes of color classes will remain the same.
+     * Use vertices from compensator to compensate difference
+     * in sizes of color classes.
+     *
+     * @param colorBig    color of bigger color class
+     * @param colorSmall  color of smaller color class
+     * @param compensator list of vertices used to compensate
+     */
     void swapWithoutDecreasing(int colorBig, int colorSmall, LinkedList<Integer> compensator) {
         BitSet checked = new BitSet(graph.getVertices());
         LinkedList<Integer> bigComponent = new LinkedList<>(), smallComponent = new LinkedList<>();
         int currentColor, sizeDifference;
 
-        for(int i = 0; i < graph.getVertices(); i++) {
+        for (int i = 0; i < graph.getVertices(); i++) {
 
             if (!checked.get(i)) {
 
@@ -95,6 +105,19 @@ class ComponentSwapper {
         }
     }
 
+    /**
+     * Try to swap components between color classes
+     * and move vertices in number of difference in sizes
+     * of those components from compensator to the third
+     * color class.
+     *
+     * @param colorBig       color of bigger color class
+     * @param colorSmall     color of smaller color class
+     * @param colorOther     color of the third color class
+     * @param compensator    list of vertices used to compensate
+     * @param verticesToMove desired change of the coloring width
+     * @return how much the width was decreased
+     */
     int swapAndMoveToOther(int colorBig, int colorSmall, int colorOther, LinkedList<Integer> compensator, int verticesToMove) {
         BitSet checked = new BitSet(graph.getVertices()), compensatorArray = new BitSet(graph.getVertices());
         LinkedList<Integer> bigComponent = new LinkedList<>(), smallComponent = new LinkedList<>(), toRemoveFromCompensator = new LinkedList<>();
@@ -104,11 +127,11 @@ class ComponentSwapper {
          * Rewrite compensator as bit array for
          * checking value in constant time.
          */
-        for(Integer i : compensator) {
+        for (Integer i : compensator) {
             compensatorArray.set(i);
         }
 
-        for(int i = 0; i < graph.getVertices(); i++) {
+        for (int i = 0; i < graph.getVertices(); i++) {
 
             if (!checked.get(i) && 0 < verticesToMove) {
 
@@ -156,14 +179,15 @@ class ComponentSwapper {
     }
 
     /**
-     * Find components within two given colors
-     * starting from vertex with current index.
+     * Find components within induced graph of
+     * two given color starting from vertex with
+     * current index.
      *
-     * @param current index to start with
-     * @param colorBig color of bigger color class
-     * @param colorSmall color of smaller color class
-     * @param bigComponent list of indexes to fill
-     * @param smallComponent list of indexes to fill
+     * @param current        index to start with
+     * @param colorBig       color of bigger color class
+     * @param colorSmall     color of smaller color class
+     * @param bigComponent   container of indexes to fill
+     * @param smallComponent container of indexes to fill
      */
     private void findComponents(int current, int colorBig, int colorSmall,
                                 LinkedList<Integer> bigComponent,
@@ -182,8 +206,7 @@ class ComponentSwapper {
             if (currentColor == colorBig) {
                 otherColor = colorSmall;
                 bigComponent.add(current);
-            }
-            else {
+            } else {
                 otherColor = colorBig;
                 smallComponent.add(current);
             }
@@ -198,11 +221,13 @@ class ComponentSwapper {
     }
 
     /**
-     * Change n vertices from the list to a given color.
+     * Change n vertices from vertices list to
+     * given color and remove them from list.
      *
      * @param vertices list of available vertices
-     * @param color that vertices will now have
-     * @param n number of vertices to change color
+     * @param color    that vertices will now have
+     * @param n        number of vertices to be changed
+     * @return number of vertices changed
      */
     int changeColor(LinkedList<Integer> vertices, int color, int n) {
         int changed = 0;
@@ -223,8 +248,8 @@ class ComponentSwapper {
      * to a given color.
      *
      * @param vertices bit set with 1 for all available vertices
-     * @param color that vertices will now have
-     * @param n number of vertices to change color
+     * @param color    that vertices will now have
+     * @param n        number of vertices to change color
      */
     private void changeColor(BitSet vertices, int color, int n) {
         for (int i = 0; i < vertices.size() && 0 < n; i++) {
